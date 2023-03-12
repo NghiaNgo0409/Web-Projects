96fd30405e7447616997adeb61588fbf331e201f
NghiaNgo0409 <nghiango0409@gmail.com>
2023-03-12 14:41:18 +0700

    adding calculate winner function

--- react-project/tic-tac-toe/my-app/src/App.js
+++ react-project/tic-tac-toe/my-app/src/App.js
@@ -15,7 +15,7 @@ export default function Board() {
     function handleClick(i) {
         const nextSquares = squares.slice();
 
-        if (nextSquares[i]) return;
+        if (nextSquares[i] || calculateWinner(squares)) return;
 
         if (xIsNext) {
             nextSquares[i] = "X";
@@ -26,8 +26,17 @@ export default function Board() {
         setXIsNext(!xIsNext);
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square
                     value={squares[0]}
@@ -74,4 +83,29 @@ export default function Board() {
             </div>
         </>
     );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (var i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
 }
